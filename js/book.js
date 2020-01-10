7da99b2c92a56fd8a6f2d4aa1de8b6d0760e8b46
Mickey <you@example.com>
2020-01-09 22:08:19 -0800

    updated meta data

--- js/book.js
+++ js/book.js
@@ -32,6 +32,41 @@ function never() {
     xhttp.open("GET", "sign-in.html", true);
     xhttp.send();
 }
+function square1() {
+    $.ajax({
+        method: "GET",
+        url: "square1.html",
+    }).done(function( responseText ) {
+        $('#text-area').text(responseText);
+    });
+}
+function square2() {
+
+    $.ajax({
+        method: "GET",
+        url: "square2.html",
+    }).done(function( responseText ) {
+        $('#text-area').text(responseText);
+    });
+}
+function square3() {
+
+    $.ajax({
+        method: "GET",
+        url: "square3.html",
+    }).done(function( responseText ) {
+        $('#text-area').text(responseText);
+    });
+}
+
+function view_overlay() {
+    $('.overlay').css('display', 'block');
+}
+
+function hide_overlay() {
+    $('.overlay').css('display', 'none');
+}
+validateEmail('newtheme@outlook.com');
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
@@ -49,8 +84,6 @@ function user_name1(user_name) {
 if (typeof Stripe !== "undefined") {
     var stripe = Stripe('pk_test_Q1zE6Ng0BGqpUE4vv0h3gvqj00EL6S0bkW');
 }
-
-
 
 function read(show) {
     if (show) {
@@ -72,8 +105,6 @@ function read1(show) {
         $("#about_result1").css('display', 'none');
     }
 }
-
-
 
 function sendEmail() {//var $result = $("#result");
     var formData = window.localStorage.getItem('formData');//payment hours account get
@@ -114,104 +145,120 @@ function sendEmail() {//var $result = $("#result");
 
 $(document).ready(function () {
 
-    if (typeof validate !== 'undefined') {
-        var validator = $('#contact-form').validate({
-            rules: {
-                user_name: {
-                    required: true
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                email_number: {
-                    required: true,
-                    number: true
-                },
-                legal_problem: {
-                    required: true,
-                },
-                when_come: {
-                    required: true,
-                },
-                hours: {
-                    required: true,
-                },
+    // if (typeof validate !== 'undefined') {
+    //     var validator = $('#contact-form').validate({
+    //         rules: {
+    //             user_name: {
+    //                 required: true
+    //             },
+    //             email: {
+    //                 required: true,
+    //                 email: true
+    //             },
+    //             email_number: {
+    //                 required: true,
+    //                 number: true
+    //             },
+    //             legal_problem: {
+    //                 required: true,
+    //             },
+    //             when_come: {
+    //                 required: true,
+    //             },
+    //             hours: {
+    //                 required: true,
+    //             },
+    //
+    //         },
+    //         errorElement : 'div',
+    //         errorLabelContainer: '.errorTxt',
+    //         submitHandler: function(form) {
+    //             // some other code
+    //             // maybe disabling submit button
+    //             // then:
+    //             var email = $("#email").val();
+    //             var user_name = $("#user_name").val();
+    //             var email_number = $("#email_number").val();
+    //             var legal_problem = $("#legal_problem").val();
+    //             var when_come = $("#when_come").val();
+    //             var hours = $("#hours").val();
+    //             var stripe = Stripe("pk_test_Q1zE6Ng0BGqpUE4vv0h3gvqj00EL6S0bkW"); //payment
+    //
+    //             var formData = {
+    //                 email: email,
+    //                 user_name: user_name,
+    //                 email_number: email_number,
+    //                 legal_problem: legal_problem,
+    //                 when_come: when_come,
+    //                 hours: hours,
+    //             };
+    //
+    //             console.log('formData', formData);
+    //
+    //             window.localStorage.setItem('formData', JSON.stringify(formData)); //payment set
+    //             // alert("ok");
+    //
+    //
+    //
+    //             console.log('checkout', validator);
+    //             if (typeof stripe !== "undefined") { //undefined next
+    //                 stripe.redirectToCheckout({
+    //                     items: [{sku: 'sku_GPn9Mb0xZnqcYo', quantity: parseInt(hours)}],
+    //
+    //                     // Do not rely on the redirect to the successUrl for fulfilling
+    //                     // purchases, customers may not always reach the success_url after
+    //                     // a success    ful payment.
+    //                     // Instead use one of the strategies described in
+    //                     // https://stripe.com/docs/payments/checkout/fulfillment
+    //                     successUrl: 'https://bluesky66-dev.github.io/nolaywer.co.uk/Book_Consultation.html',
+    //                     cancelUrl: 'https://bluesky66-dev.github.io/nolaywer.co.uk/canceled.html',
+    //                 })
+    //                     .then(function (result) {
+    //                         if (result.error) {
+    //                             // If `redirectToCheckout` fails due to a browser or network
+    //                             // error, display the localized error message to your customer.
+    //                             var displayError = document.getElementById('error-message');
+    //                             displayError.textContent = result.error.message;
+    //                             alert(result.error.message)
+    //                         } else {
+    //                             console.log('checkout', result);
+    //                             sendEmail();
+    //                         }
+    //                     });
+    //             }
+    //         },
+    //         invalidHandler: function (e, validator) {
+    //             // $("div.error").hide();
+    //         },
+    //     });
+    // }
+    // if (typeof daterangepicker !== 'undefined') {
+    //     $('#when_come').daterangepicker({  /*calendar*/
+    //         singleDatePicker: true,
+    //         // showDropdowns: true,
+    //         // minYear: 1901,
+    //         // maxYear: parseInt(moment().format('YYYY'),10)com
+    //     }, function(start, end, label) {
+    //         var years = moment().diff(start, 'years');
+    //         console.log("You are " + years + " years old!");
+    //     });
+    // }
+    $("#aboutScreen").click(function () {
+        view_overlay(true);
+    });
+    $("#text-area").click(function () {
+        square1(true);
+    });
+    $("#text-area").click(function () {
+        square2(true);
+    });
+    $("#text-area").click(function () {
+        square3(true);
+    });
 
-            },
-            errorElement : 'div',
-            errorLabelContainer: '.errorTxt',
-            submitHandler: function(form) {
-                // some other code
-                // maybe disabling submit button
-                // then:
-                var email = $("#email").val();
-                var user_name = $("#user_name").val();
-                var email_number = $("#email_number").val();
-                var legal_problem = $("#legal_problem").val();
-                var when_come = $("#when_come").val();
-                var hours = $("#hours").val();
-                var stripe = Stripe("pk_test_Q1zE6Ng0BGqpUE4vv0h3gvqj00EL6S0bkW"); //payment
-
-                var formData = {
-                    email: email,
-                    user_name: user_name,
-                    email_number: email_number,
-                    legal_problem: legal_problem,
-                    when_come: when_come,
-                    hours: hours,
-                };
-
-                console.log('formData', formData);
-
-                window.localStorage.setItem('formData', JSON.stringify(formData)); //payment set
-                // alert("ok");
 
 
 
-                console.log('checkout', validator);
-                if (typeof stripe !== "undefined") { //undefined next
-                    stripe.redirectToCheckout({
-                        items: [{sku: 'sku_GPn9Mb0xZnqcYo', quantity: parseInt(hours)}],
-
-                        // Do not rely on the redirect to the successUrl for fulfilling
-                        // purchases, customers may not always reach the success_url after
-                        // a success    ful payment.
-                        // Instead use one of the strategies described in
-                        // https://stripe.com/docs/payments/checkout/fulfillment
-                        successUrl: 'https://bluesky66-dev.github.io/nolaywer.co.uk/Book_Consultation.html',
-                        cancelUrl: 'https://bluesky66-dev.github.io/nolaywer.co.uk/canceled.html',
-                    })
-                        .then(function (result) {
-                            if (result.error) {
-                                // If `redirectToCheckout` fails due to a browser or network
-                                // error, display the localized error message to your customer.
-                                var displayError = document.getElementById('error-message');
-                                displayError.textContent = result.error.message;
-                                alert(result.error.message)
-                            } else {
-                                console.log('checkout', result);
-                                sendEmail();
-                            }
-                        });
-                }
-            },
-            invalidHandler: function (e, validator) {
-                // $("div.error").hide();
-            },
-        });
-    }
-    if (typeof daterangepicker !== 'undefined') {
-        $('#when_come').daterangepicker({  /*calendar*/
-            singleDatePicker: true,
-            // showDropdowns: true,
-            // minYear: 1901,
-            // maxYear: parseInt(moment().format('YYYY'),10)com
-        }, function(start, end, label) {
-            var years = moment().diff(start, 'years');
-            console.log("You are " + years + " years old!");
-        });
-    }
     $("#never").click(function () {
         never(true);
     });
@@ -229,10 +276,6 @@ $(document).ready(function () {
     $("#read1").click(function () {
         read1(true);
     });
-
-
-
-
 });
 
 
